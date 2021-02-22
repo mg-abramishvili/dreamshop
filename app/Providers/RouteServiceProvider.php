96e05b97670bd557f8549787f77f9049ceadf42e
mg-abramishvili <mg@abramishvili.net>
2021-02-22 13:41:16 +0500

    .

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/backend/dashboard';
 
     /**
      * The controller namespace for the application.
